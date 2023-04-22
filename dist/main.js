9dd74c55a8fe6229d828737376b7e007e2a11c55
SCJAB <zzep96@gmail.com>
2023-04-22 17:56:40 +0800

    Update

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,5 @@
+// 1. Construct if and if ...else
+
 let UI_age = prompt("Enter your age: ")
 
 if (UI_age >= 25)
@@ -9,3 +11,32 @@ else
 {
     alert("Sorry, you need to be 25 years old and up.");
 }
+
+// 2. Construct for loops
+
+// Condition 1
+for (let i = 0; i <= 30; i++)
+{
+    console.log(i);
+}
+console.log("Done");
+
+// Condition 2
+for (let i = 0; i <= 40; i += 2) 
+{
+    if (i % 2 === 0) 
+    {
+      console.log(i);
+    }
+}
+console.log("Done");
+
+// Condition 3
+for (let i = 40; i >= 10; i--) 
+{
+    if (i % 3 === 0) 
+    {
+      console.log(i);
+    }
+}
+console.log("Done");
